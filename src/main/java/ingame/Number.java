c9e9cbcad293ea5a131ffc5587c0c44d838b3260
tributetothemoon <kkm97351@gmail.com>
2020-08-09 04:00:35 +0900

    feat : add constructor of number class The constructor of number class has been updated. Function overloading has been used.

--- src/main/java/ingame/Number.java
+++ src/main/java/ingame/Number.java
@@ -1,5 +1,28 @@
 package ingame;
 
+import java.util.Random;
+
 public class Number {
     private int[] number = new int[3];
+    Random intGenerator = new Random();
+
+    public Number(){
+        generateRandomNumber();
+    }
+
+    public Number(int[] input){
+        System.arraycopy(input, 0, this.number, 0, 3);
+    }
+
+    void generateRandomNumber(){
+        //generating random number
+        for (int i = 0; i < 3; i++) {
+            this.number[i] = intGenerator.nextInt(9) + 1;
+            for (int j = 0; j < i; j++) {   //중복 제거
+                if (this.number[j] == this.number[i]) i--;
+            }
+        }
+
+    }
 }
+
